170d7d46777e5068b54c1f6724f4c3f30ae509da
khaliq Farah <abdulfarah567@gmail.com>
2024-09-20 17:22:20 -0600

    face-tracking-script.py

--- face-tracking-script.py
+++ face-tracking-script.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-# Load the pre-trained face detection model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Initialize the video capture
